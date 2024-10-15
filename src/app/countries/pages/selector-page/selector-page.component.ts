01a4da7b8173335b11ae749235ad81eec4b3fad8
Alan Jeria Medina <alanjeria01@gmail.com>
2024-10-15 13:48:35 -0300

    convertir pais de la interfaz y mostrarlos

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
-import { Region } from '../../interfaces/country.interfaces';
+import { Region, SmallCountry } from '../../interfaces/country.interfaces';
 import { switchMap } from 'rxjs';
 
 @Component({
@@ -12,6 +12,7 @@ import { switchMap } from 'rxjs';
 export class SelectorPageComponent implements OnInit{
 
   public myForm: FormGroup;
+  public countriesByRegion : SmallCountry[] = [];
 
 
   constructor(
@@ -43,8 +44,8 @@ export class SelectorPageComponent implements OnInit{
       .pipe(
         switchMap( region => this.countriesService.getCountriesByRegion( region ) )
       )
-    .subscribe( value =>{
-      console.log({ region: value });
+    .subscribe( countries =>{
+      this.countriesByRegion = countries;
     })
   }
 
