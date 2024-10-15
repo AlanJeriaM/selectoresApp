0980579c14f6137c4567fbff7bb7338e798eb2d1
Alan Jeria Medina <alanjeria01@gmail.com>
2024-10-15 13:18:51 -0300

    creacion de interfaz SmallCountry y del selector anidado de paises

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interfaces';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styles: ``
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit{
 
   public myForm: FormGroup;
 
@@ -28,9 +29,23 @@ export class SelectorPageComponent {
 
   }
 
+  ngOnInit(): void {
+    this.onRegionChanged();
+  }
+
   get regions(): Region[] {
     return this.countriesService.regions;
 
+  }
+
+  onRegionChanged(): void {
+    this.myForm.get('region')!.valueChanges
+      .pipe(
+        switchMap( region => this.countriesService.getCountriesByRegion( region ) )
+      )
+    .subscribe( value =>{
+      console.log({ region: value });
+    })
   }
 
 
